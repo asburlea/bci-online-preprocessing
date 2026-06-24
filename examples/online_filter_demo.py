47a5d4c5d5832d25c81e55add1e3cd34edddad52
asburlea <andreea.sburlea@yahoo.com>
2026-06-24 15:24:47 +0200

    resove_byprop

--- examples/online_filter_demo.py
+++ examples/online_filter_demo.py
@@ -3,13 +3,13 @@ Visualize filtered EEG in real time
 """
 
 import matplotlib.pyplot as plt
-from pylsl import StreamInlet, resolve_stream
+from pylsl import StreamInlet, resolve_byprop
 from src.filters import OnlineBandpass
 
 FS = 250
 N_CHANNELS = 8
 
-streams = resolve_stream("type", "EEG")
+streams = resolve_byprop("type", "EEG", 5)
 inlet = StreamInlet(streams[0])
 filt = OnlineBandpass(FS, 8, 30, N_CHANNELS)
 
